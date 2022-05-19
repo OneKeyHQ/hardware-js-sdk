5de4e9389c3c80af0aaeb4b386a798e533cf5c64
Leon <lixiao.dev@gmail.com>
2022-05-19 22:07:21 +0800

    wip: change bridge uri && config example project cros

--- packages/hd-transport-http/src/constants.ts
+++ packages/hd-transport-http/src/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_URL = 'http://127.0.0.1:21325';
+export const DEFAULT_URL = 'http://localhost:21320';
 export const DEFAULT_VERSION_URL = 'https://connect.onekey.so/8/data/bridge/latest.txt';
