885fe473025212e0f7c480027bad95f6753e7380
Leon <lixiao.dev@gmail.com>
2022-05-22 12:12:48 +0800

    wip: remove debugLink

--- packages/hd-transport/src/types/transport.ts
+++ packages/hd-transport/src/types/transport.ts
@@ -18,17 +18,12 @@ export type MessageFromOneKey = { type: string; message: Record<string, any> };
 export type Transport = {
   enumerate(): Promise<Array<OneKeyDeviceInfoWithSession>>;
   listen(old?: Array<OneKeyDeviceInfoWithSession>): Promise<Array<OneKeyDeviceInfoWithSession>>;
-  acquire(input: AcquireInput, debugLink: boolean): Promise<string>;
-  release(session: string, onclose: boolean, debugLink: boolean): Promise<void>;
+  acquire(input: AcquireInput): Promise<string>;
+  release(session: string, onclose: boolean): Promise<void>;
   configure(signedData: JSON | string): Promise<void>;
-  call(
-    session: string,
-    name: string,
-    data: Record<string, any>,
-    debugLink: boolean
-  ): Promise<MessageFromOneKey>;
-  post(session: string, name: string, data: Record<string, any>, debugLink: boolean): Promise<void>;
-  read(session: string, debugLink: boolean): Promise<MessageFromOneKey>;
+  call(session: string, name: string, data: Record<string, any>): Promise<MessageFromOneKey>;
+  post(session: string, name: string, data: Record<string, any>): Promise<void>;
+  read(session: string): Promise<MessageFromOneKey>;
 
   // resolves when the transport can be used; rejects when it cannot
   init(debug?: boolean): Promise<void>;
