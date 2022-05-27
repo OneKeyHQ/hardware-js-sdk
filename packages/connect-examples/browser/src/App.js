6b4ab01b5904c4778494d05b69c84fe5e39d71fd
Leon <lixiao.dev@gmail.com>
2022-05-27 14:59:36 +0800

    wip: update example page

--- packages/connect-examples/browser/src/App.js
+++ packages/connect-examples/browser/src/App.js
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react'
 import HardwareWebSdk from '@onekeyfe/hd-web-sdk'
 import './App.css';
 
-let core
-
 let loaded = false
 
 function App() {
@@ -26,17 +24,6 @@ function App() {
     loaded = true
   }, []);
 
-  const callAPI = async () => {
-    const res = await HardwareWebSdk.call({method: 'getFeatures', params: {foo: 'bar'}})
-    console.log('react call api response: ', res)
-  }
-
-  const usbConnect = () => {
-    if (currentDevice) {
-      core.initDevice(currentDevice.path)
-    }
-  }
-
   const getDevicesList = async () => {
    const res = await HardwareWebSdk.searchDevices()
    console.log('react searchDevices response: ', res)
@@ -54,9 +41,7 @@ function App() {
       <div>
         <button onClick={sdkInit}>SDK init</button>
         <button onClick={getDevicesList}>GetDeviceList</button>
-        <button onClick={usbConnect}>Device Connect</button>
         <button onClick={onGetFeatures}>GetFeatures</button>
-        <button onClick={callAPI}>CallAPI</button>
       </div>
       <div style={{textAlign: 'left', margin: '20px'}}>
         当前选取设备：{currentDevice ? JSON.stringify(currentDevice) : '无'}
