fdaa405168ec993d30e6d3a3f2147cb7a45b6821
zhao.zhang <loatheb.zhao@gmail.com>
2022-05-31 11:44:56 +0800

    fix: remove css block style

--- packages/hd-web-sdk/src/iframe/builder.ts
+++ packages/hd-web-sdk/src/iframe/builder.ts
@@ -1,6 +1,5 @@
 import { ERRORS, Deferred, create as createDeferred } from '@onekeyfe/hd-core';
 import { getOrigin } from '../utils/urlUtils';
-import css from './inlineStyles';
 
 /* eslint-disable import/no-mutable-exports */
 export let instance: HTMLIFrameElement | null;
@@ -8,27 +7,6 @@ export let origin: string;
 export let initPromise: Deferred<void> = createDeferred();
 export let timeout = 0;
 /* eslint-disable import/no-mutable-exports */
-
-const injectStyleSheet = () => {
-  if (!instance) {
-    throw ERRORS.TypedError('Init_IframeBlocked');
-  }
-  const doc = instance.ownerDocument;
-  const head = doc.head || doc.getElementsByTagName('head')[0];
-  const style = document.createElement('style');
-  style.setAttribute('type', 'text/css');
-  style.setAttribute('id', 'TrezorConnectStylesheet');
-
-  // @ts-ignore
-  if (style.styleSheet) {
-    // @ts-ignore
-    style.styleSheet.cssText = css;
-    head.appendChild(style);
-  } else {
-    style.appendChild(document.createTextNode(css));
-    head.append(style);
-  }
-};
 
 export const init = async (settings: any) => {
   initPromise = createDeferred();
@@ -48,10 +26,7 @@ export const init = async (settings: any) => {
     instance.id = 'onekey-connect';
   }
 
-  const manifestString = settings.manifest ? JSON.stringify(settings.manifest) : 'undefined'; // note: btoa(undefined) === btoa('undefined') === "dW5kZWZpbmVk"
-  const manifest = `version=${settings.version as string}&manifest=${encodeURIComponent(
-    btoa(JSON.stringify(manifestString))
-  )}`;
+  const manifest = `version=${settings.version as string}`;
   const src = `${settings.iframeSrc as string}?${manifest}`;
 
   instance.setAttribute('src', src);
@@ -91,8 +66,6 @@ export const init = async (settings: any) => {
   // inject iframe into host document body
   if (document.body) {
     document.body.appendChild(instance);
-    // eslint-disable-next-line no-use-before-define
-    injectStyleSheet();
   }
 
   try {
