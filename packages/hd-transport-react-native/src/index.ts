a462b7b7a8795626adda7dcc245f334b27008df0
Leon <lixiao.dev@gmail.com>
2022-06-01 22:51:59 +0800

    feat: subscribe ble state

--- packages/hd-transport-react-native/src/index.ts
+++ packages/hd-transport-react-native/src/index.ts
@@ -1,1 +1,25 @@
-export default {};
+import transport from '@onekeyfe/hd-transport';
+
+const { check, buildOne, receiveOne, parseConfigure } = transport;
+
+export default class ReactNativeTransport {
+  _messages: ReturnType<typeof transport.parseConfigure> | undefined;
+
+  configured = false;
+
+  stopped = false;
+
+  init() {
+    // empty
+  }
+
+  configure(signedData: any) {
+    const messages = parseConfigure(signedData);
+    this.configured = true;
+    this._messages = messages;
+  }
+
+  listen() {
+    // empty
+  }
+}
