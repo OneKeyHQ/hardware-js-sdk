a62baf3c915eb1fb9620c92fd9b7e93352972f3c
Leon <lixiao.dev@gmail.com>
2022-06-06 08:56:36 +0800

    feat: add hd-ble-sdk package

---
+++ packages/hd-ble-sdk/src/index.ts
@@ -0,0 +1,38 @@
+import EventEmitter from 'events';
+import HardwareSdk, {
+  ConnectSettings,
+  enableLog,
+  initLog,
+  parseConnectSettings,
+} from '@onekeyfe/hd-core';
+
+const eventEmitter = new EventEmitter();
+const Log = initLog('@onekey/hd-ble-sdk');
+
+let _settings = parseConnectSettings();
+
+const dispose = () => {
+  eventEmitter.removeAllListeners();
+  _settings = parseConnectSettings();
+};
+
+const init = (settings: Partial<ConnectSettings>) => {
+  _settings = { ..._settings, ...settings };
+
+  enableLog(!!settings.debug);
+  Log.debug('init');
+};
+
+const call = (params: any) => {
+  Log.debug('call: ', params);
+  return Promise.resolve();
+};
+
+const HardwareBleSdk = HardwareSdk({
+  eventEmitter,
+  init,
+  call,
+  dispose,
+});
+
+export default HardwareBleSdk;
