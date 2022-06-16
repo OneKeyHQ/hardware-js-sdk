78126ba86c30ae3c13b516bd6f3fa4baec98b10e
Leon <lixiao.dev@gmail.com>
2022-06-16 11:52:53 +0800

    feat: ui response

--- packages/core/src/events/ui-request.ts
+++ packages/core/src/events/ui-request.ts
@@ -1,12 +1,22 @@
 import type { PROTO } from '../constants';
 import type { Device } from '../types';
+import type { DeviceButtonRequest } from './device';
 import type { MessageFactoryFn } from './utils';
 
 export const UI_EVENT = 'UI_EVENT';
 
 export const UI_REQUEST = {
   REQUEST_PIN: 'ui-request_pin',
+  INVALID_PIN: 'ui-invalid_pin',
+  REQUEST_BUTTON: 'ui-button',
+
+  CLOSE_UI_WINDOW: 'ui-close_window',
 } as const;
+
+export interface UiRequestWithoutPayload {
+  type: typeof UI_REQUEST.CLOSE_UI_WINDOW;
+  payload?: typeof undefined;
+}
 
 export type UiRequestDeviceAction = {
   type: typeof UI_REQUEST.REQUEST_PIN;
@@ -16,7 +26,12 @@ export type UiRequestDeviceAction = {
   };
 };
 
-export type UiEvent = UiRequestDeviceAction;
+export interface UiRequestButton {
+  type: typeof UI_REQUEST.REQUEST_BUTTON;
+  payload: DeviceButtonRequest['payload'];
+}
+
+export type UiEvent = UiRequestWithoutPayload | UiRequestDeviceAction | UiRequestButton;
 
 export type UiEventMessage = UiEvent & { event: typeof UI_EVENT };
 
