a15f8d4666f8bdab8eb818946fb989007b09a4e6
Leon <lixiao.dev@gmail.com>
2022-07-05 09:41:16 +0800

    feat: HardwareError

---
+++ packages/shared/src/HardwareError.ts
@@ -0,0 +1,43 @@
+export interface IHardwareError {
+  errorCode: ValueOf<typeof HardwareErrorCode>;
+  message: string;
+}
+
+type ValueOf<P extends object> = P[keyof P];
+
+export type HardwareErrorCodeMessageMapping = { [P in ValueOf<typeof HardwareErrorCode>]: string };
+
+export class HardwareError extends Error {
+  errorCode: ValueOf<typeof HardwareErrorCode> = HardwareErrorCode.UnknownError;
+
+  message = '';
+
+  constructor(
+    hardwareError: IHardwareError | string,
+    errorMessageMapping: HardwareErrorCodeMessageMapping
+  ) {
+    super();
+    this.message = errorMessageMapping[HardwareErrorCode.UnknownError];
+
+    if (typeof hardwareError === 'string') {
+      this.errorCode = HardwareErrorCode.UnknownError;
+      this.message = hardwareError;
+    } else {
+      const message = errorMessageMapping[hardwareError.errorCode];
+      if (message) {
+        this.message = fillStringWithArguments(message, hardwareError);
+      }
+      this.errorCode = hardwareError.errorCode;
+    }
+
+    this.name = 'HardwareError';
+  }
+}
+
+export const HardwareErrorCode = {
+  UnknownError: 0,
+} as const;
+
+export function fillStringWithArguments(value: string, object: object) {
+  return value.replace(/\{([^}]+)\}/g, (_, arg: string) => (object as unknown as any)[arg] || '?');
+}
