c67119f422c0a40f1e7c139975f64f23856c040c
Leon <lixiao.dev@gmail.com>
2022-08-13 10:43:31 +0800

    feat: webusb transport

--- packages/hd-transport-webusb/src/index.ts
+++ packages/hd-transport-webusb/src/index.ts
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 import transport, { AcquireInput } from '@onekeyfe/hd-transport';
 import { ERRORS, HardwareErrorCode } from '@onekeyfe/hd-shared';
+import ByteBuffer from 'bytebuffer';
 
-const { parseConfigure } = transport;
+const { parseConfigure, buildEncodeBuffers, decodeProtocol, receiveOne, check } = transport;
 
 const ONEKEY_FILTER = [
   { vendorId: 0x1209, productId: 0x53c0 },
@@ -11,6 +13,8 @@ const ONEKEY_FILTER = [
 const CONFIGURATION_ID = 1;
 const INTERFACE_ID = 0;
 const ENDPOINT_ID = 1;
+const PACKET_SIZE = 64;
+const HEADER_LENGTH = 6;
 
 export default class WebUsbTransport {
   _messages: ReturnType<typeof transport.parseConfigure> | undefined;
@@ -126,6 +130,95 @@ export default class WebUsbTransport {
     await device.claimInterface(this.interfaceId);
   }
 
-  async release()
+  async call(path: string, name: string, data: Record<string, unknown>) {
+    if (this._messages == null) {
+      throw ERRORS.TypedError(HardwareErrorCode.TransportNotConfigured);
+    }
+
+    const device = await this._findDevice(path);
+    if (!device) {
+      throw ERRORS.TypedError(HardwareErrorCode.DeviceNotFound);
+    }
+
+    const messages = this._messages;
+    this.Log.debug('call-', ' name: ', name, ' data: ', data);
+    const encodeBuffers = buildEncodeBuffers(messages, name, data);
+
+    for (const buffer of encodeBuffers) {
+      const newArray: Uint8Array = new Uint8Array(PACKET_SIZE);
+      newArray[0] = 63;
+      newArray.set(new Uint8Array(buffer), 1);
+      console.log('send packet: ', newArray);
+      if (!device.opened) {
+        await this.connect(path, false);
+      }
+      await device.transferOut(this.endpointId, newArray);
+    }
+
+    const resData = await this._receive(path);
+    if (typeof resData !== 'string') {
+      throw ERRORS.TypedError(HardwareErrorCode.NetworkError, 'Returning data is not string.');
+    }
+    const jsonData = receiveOne(messages, resData);
+    return check.call(jsonData);
+  }
+
+  async _receive(path: string) {
+    const device: USBDevice = await this._findDevice(path);
+    if (!device.opened) {
+      await this.connect(path, false);
+    }
+
+    const firstPacket = await device.transferIn(this.endpointId, PACKET_SIZE);
+    console.log('receive first packet: ', firstPacket);
+    const firstData = firstPacket.data?.buffer.slice(1);
+    const { length, typeId, restBuffer } = decodeProtocol.decodeChunked(firstData as ArrayBuffer);
+
+    console.log('chunk length: ', length);
+
+    const lengthWithHeader = Number(length + HEADER_LENGTH);
+    const decoded = new ByteBuffer(lengthWithHeader);
+    decoded.writeUint16(typeId);
+    decoded.writeUint32(length);
+    if (length) {
+      decoded.append(restBuffer);
+    }
+
+    console.log('first decoded: ', decoded);
+
+    while (decoded.offset < lengthWithHeader) {
+      const res = await device.transferIn(this.endpointId, PACKET_SIZE);
+      console.log('otherRes data: ', res?.data?.buffer);
+      console.log('otherRes length: ', res?.data?.byteLength);
+
+      if (!res.data) {
+        throw new Error('no data');
+      }
+      if (res.data.byteLength === 0) {
+        // empty data
+        console.warn('empty data');
+      }
+      const buffer = res.data.buffer.slice(1);
+      if (lengthWithHeader - decoded.offset >= PACKET_SIZE) {
+        decoded.append(buffer);
+      } else {
+        decoded.append(buffer.slice(0, lengthWithHeader - decoded.offset));
+      }
+      console.log('current offset: ', decoded.offset);
+    }
+    decoded.reset();
+    const result = decoded.toBuffer();
+    return Buffer.from(result).toString('hex');
+  }
+
+  async release(path: string) {
+    const device: USBDevice = await this._findDevice(path);
+    await device.releaseInterface(this.interfaceId);
+    await device.close();
+  }
+
+  async requestDevice() {
+    if (!this.usb) return;
+    await this.usb.requestDevice({ filters: ONEKEY_FILTER });
+  }
 }
-export {};
