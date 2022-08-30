ca8cbe01163a53a988e3fef8af1edee57b06b93c
ByteZhang1024 <ByteZhang@protonmail.com>
2022-08-30 20:39:28 +0800

    Revert "Test: @onekeyfe/transport add unit test (#125)"
    This reverts commit 7998d4db8dd11fd8c0fe13e24b677ae8e4a91ca7.

--- packages/hd-transport/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: '../../jest.config.js',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['node_modules', '<rootDir>/lib', '<rootDir>/libDev'],
-  collectCoverage: true,
-  // collectCoverageFrom: ['src/**/*.ts'],
-};
