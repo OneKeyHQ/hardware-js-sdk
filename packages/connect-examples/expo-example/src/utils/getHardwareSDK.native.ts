231f3773872737ca1ac6e0b93a5997850bc27baa
Leon <lixiao.dev@gmail.com>
2022-10-20 13:01:42 +0800

    fix: import sdk with native specific extensions

---
+++ packages/connect-examples/expo-example/src/utils/getHardwareSDK.native.ts
@@ -0,0 +1,3 @@
+import HardwareSDK from '@onekeyfe/hd-ble-sdk';
+
+export const getHardwareSDK = async () => Promise.resolve(HardwareSDK);
--- packages/connect-examples/expo-example/src/utils/hardwareInstance.ts
+++ packages/connect-examples/expo-example/src/utils/hardwareInstance.ts
@@ -1,11 +1,11 @@
 import memoizee from 'memoizee';
 import { Platform } from 'react-native';
 import type { ConnectSettings, CoreApi } from '@onekeyfe/hd-core';
+import { getHardwareSDK } from './getHardwareSDK';
 
 // eslint-disable-next-line import/no-mutable-exports
 let HardwareSDK: CoreApi;
 let initialized = false;
-const isNodeEnvironments = false;
 
 export const getHardwareSDKInstance = memoizee(
   async () =>
@@ -20,13 +20,9 @@ export const getHardwareSDKInstance = memoizee(
         debug: true,
       };
 
-      if (Platform.OS === 'android' || Platform.OS === 'ios') {
-        HardwareSDK = (await import('@onekeyfe/hd-ble-sdk')).default as unknown as CoreApi;
-      } else if (isNodeEnvironments) {
-        HardwareSDK = (await import('@onekeyfe/hd-common-connect-sdk'))
-          .default as unknown as CoreApi;
-      } else {
-        HardwareSDK = (await import('@onekeyfe/hd-web-sdk')).default as unknown as CoreApi;
+      HardwareSDK = await getHardwareSDK();
+
+      if (Platform.OS === 'web') {
         settings.connectSrc = 'https://localhost:8087/';
         settings.env = 'web';
       }
