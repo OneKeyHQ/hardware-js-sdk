1b10d241aefb9da4344f581674d18d393cc2c4f6
Leon <lixiao.dev@gmail.com>
2022-11-16 16:34:39 +0800

    fix: compile example project on native

--- packages/connect-examples/expo-example/metro.config.js
+++ packages/connect-examples/expo-example/metro.config.js
@@ -2,4 +2,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  crypto: require.resolve('react-native-crypto'),
+  stream: require.resolve('readable-stream'),
+};
+
+module.exports = config;
