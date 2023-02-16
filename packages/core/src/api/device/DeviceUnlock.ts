53e6ddfe93ccf3878d6763b05efdee3a6348d2aa
Quincy <quincysx@onekey.so>
2023-02-16 18:26:01 +0800

    feat: support unlock

---
+++ packages/core/src/api/device/DeviceUnlock.ts
@@ -0,0 +1,29 @@
+import { ERRORS, HardwareErrorCode } from '@onekeyfe/hd-shared';
+import { UI_REQUEST } from '../../constants/ui-request';
+import { getPassphraseState } from '../../utils/deviceFeaturesUtils';
+import { BaseMethod } from '../BaseMethod';
+
+export default class DeviceUnlock extends BaseMethod {
+  init() {
+    this.allowDeviceMode = [...this.allowDeviceMode, UI_REQUEST.INITIALIZE];
+    this.useDevicePassphraseState = false;
+  }
+
+  async run() {
+    if (!this.device.features)
+      return Promise.reject(ERRORS.TypedError(HardwareErrorCode.DeviceInitializeFailed));
+
+    let { features } = this.device;
+    const locked = this.device?.features?.unlocked === true;
+
+    if (!locked) {
+      await getPassphraseState(this.device.features, this.device.commands);
+    }
+
+    if (locked) {
+      const { message } = await this.device.commands.typedCall('GetFeatures', 'Features', {});
+      features = message;
+    }
+    return Promise.resolve(features);
+  }
+}
