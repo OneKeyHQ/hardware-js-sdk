332cdcbbe19a584e8b36151abecef76897d31130
Leon <lixiao@onekey.so>
2023-02-22 16:08:43 +0800

    chore: add comment

--- packages/core/src/api/firmware/updateBootloader.ts
+++ packages/core/src/api/firmware/updateBootloader.ts
@@ -12,8 +12,11 @@ export function checkNeedUpdateBoot(features: Features) {
   const currentVersion = getDeviceFirmwareVersion(features).join('.');
   const bootloaderVersion = getDeviceBootloaderVersion(features).join('.');
   return (
+    // support ResourceUpdate version 3.2.0
     semver.gte(currentVersion, '3.2.0') &&
+    // support update bootloader version 4.1.0
     semver.gte(currentVersion, '4.1.0') &&
+    // target bootloader version
     semver.lte(bootloaderVersion, '2.4.0')
   );
 }
