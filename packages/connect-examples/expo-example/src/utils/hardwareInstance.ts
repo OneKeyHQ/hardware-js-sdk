c9472ee48200f6d2e970b36a91b48bacf3e310cf
Leon <lixiao@onekey.so>
2023-07-12 15:08:47 +0800

    fix: instance url

--- packages/connect-examples/expo-example/src/utils/hardwareInstance.ts
+++ packages/connect-examples/expo-example/src/utils/hardwareInstance.ts
@@ -34,10 +34,10 @@ export const getHardwareSDKInstance = memoizee(
         console.log(HardwareSDK);
 
         if (Platform.OS === 'web') {
-          // settings.connectSrc = 'https://jssdk.onekey.so/0.3.14/';
-          settings.connectSrc = 'https://localhost:8087/';
+          settings.connectSrc = 'https://jssdk.onekey.so/0.3.15/';
+          // settings.connectSrc = 'https://localhost:8087/';
           settings.env = 'web';
-          settings.preRelease = true;
+          settings.preRelease = false;
           // HardwareLowLevelSDK = await importLowLevelSDK();
         }
 
