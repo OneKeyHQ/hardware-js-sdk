272c8fb012633410aadd9c8d5d01ce28c4712206
ByteZhang <quincysx@onekey.so>
2024-02-24 12:51:44 +0800

    chore: optimize sdk event

--- packages/connect-examples/expo-example/src/components/HandleSDKEvents.tsx
+++ packages/connect-examples/expo-example/src/components/HandleSDKEvents.tsx
@@ -1,14 +1,16 @@
-import { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import {
   CoreMessage,
   DEVICE,
   FIRMWARE_EVENT,
+  LOG_EVENT,
   UI_EVENT,
   UI_REQUEST,
   UI_RESPONSE,
+  supportInputPinOnSoftware,
 } from '@onekeyfe/hd-core';
 
-import { useIsFocused } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 import { View } from 'tamagui';
 import HardwareSDKContext from '../provider/HardwareSDKContext';
 import { ReceivePin } from './ReceivePin';
@@ -18,79 +20,6 @@ let registerListener = false;
 export default function HandleSDKEvents() {
   const { sdk: SDK, lowLevelSDK: HardwareLowLevelSDK, type } = useContext(HardwareSDKContext);
   const [showPinInput, setShowPinInput] = useState(false);
-  const [pinValue, setPinValue] = useState('');
-
-  const focus = useIsFocused();
-  const focusRef = useRef<boolean>(false);
-
-  useEffect(() => {
-    focusRef.current = focus;
-  }, [focus]);
-
-  useEffect(() => {
-    // 监听 SDK 事件
-    if (registerListener) {
-      return;
-    }
-    if (!SDK) return;
-
-    HardwareLowLevelSDK?.addHardwareGlobalEventListener(params => {
-      SDK.emit?.(params.event, { ...params });
-    });
-
-    SDK.on(UI_EVENT, (message: CoreMessage) => {
-      if (!focusRef.current) return;
-      console.log('TopLEVEL EVENT ===>>>>: ', message);
-      if (message.type === UI_REQUEST.REQUEST_PIN) {
-        // setShowPinInput(true);
-        SDK.uiResponse({
-          type: UI_RESPONSE.RECEIVE_PIN,
-          payload: '@@ONEKEY_INPUT_PIN_IN_DEVICE',
-        });
-      }
-      if (message.type === UI_REQUEST.REQUEST_PASSPHRASE) {
-        setTimeout(() => {
-          SDK.uiResponse({
-            type: UI_RESPONSE.RECEIVE_PASSPHRASE,
-            payload: {
-              value: '',
-              passphraseOnDevice: true,
-              save: false,
-            },
-          });
-        }, 2000);
-      }
-    });
-
-    // SDK.on(LOG_EVENT, (message: CoreMessage) => {
-    //   if (Array.isArray(message.payload)) {
-    //     const msg = message.payload.join(' ');
-    //     console.log('receive log event: ', msg);
-    //   }
-    // });
-
-    SDK.on(FIRMWARE_EVENT, (message: CoreMessage) => {
-      console.log('example get firmware event: ', message);
-    });
-
-    SDK.on(DEVICE.FEATURES, (message: CoreMessage) => {
-      console.log('example get features event: ', message);
-    });
-
-    SDK.on(DEVICE.CONNECT, (message: CoreMessage) => {
-      console.log('example get connect event: ', message);
-    });
-
-    SDK.on(DEVICE.DISCONNECT, (message: CoreMessage) => {
-      console.log('example get disconnect event: ', message);
-    });
-    registerListener = true;
-
-    return () => {
-      registerListener = false;
-      SDK.removeAllListeners();
-    };
-  }, [SDK, HardwareLowLevelSDK, focus]);
 
   // 输入 pin 码的确认回调
   const onConfirmPin = useCallback(
@@ -106,16 +35,94 @@ export default function HandleSDKEvents() {
     SDK?.cancel('pin-cancelled');
   }, [SDK]);
 
+  // input pin on device
+  const onInputPinOnDeviceCallback = useCallback(() => {
+    SDK?.uiResponse({
+      type: UI_RESPONSE.RECEIVE_PIN,
+      payload: '@@ONEKEY_INPUT_PIN_IN_DEVICE',
+    });
+  }, [SDK]);
+
+  useFocusEffect(
+    useCallback(() => {
+      // 监听 SDK 事件
+      if (registerListener) {
+        return;
+      }
+      if (!SDK) return;
+
+      HardwareLowLevelSDK?.addHardwareGlobalEventListener(params => {
+        SDK.emit?.(params.event, { ...params });
+      });
+
+      const uiEventCallback = (message: CoreMessage) => {
+        console.log('TopLEVEL EVENT (Api Payload)===>>>>: ', message);
+        if (message.type === UI_REQUEST.REQUEST_PIN) {
+          if (supportInputPinOnSoftware(message.payload.device.features).support) {
+            setShowPinInput(true);
+          } else {
+            onInputPinOnDeviceCallback();
+          }
+        }
+        if (message.type === UI_REQUEST.REQUEST_PASSPHRASE) {
+          setTimeout(() => {
+            SDK.uiResponse({
+              type: UI_RESPONSE.RECEIVE_PASSPHRASE,
+              payload: {
+                value: '',
+                passphraseOnDevice: true,
+                save: false,
+              },
+            });
+          }, 2000);
+        }
+      };
+      SDK.on(UI_EVENT, uiEventCallback);
+
+      // SDK.on(LOG_EVENT, (message: CoreMessage) => {
+      //   if (Array.isArray(message.payload)) {
+      //     const msg = message.payload.join(' ');
+      //     console.log('receive log event: ', msg);
+      //   }
+      // });
+
+      SDK.on(FIRMWARE_EVENT, (message: CoreMessage) => {
+        console.log('example get firmware event: ', message);
+      });
+
+      SDK.on(DEVICE.FEATURES, (message: CoreMessage) => {
+        console.log('example get features event: ', message);
+      });
+
+      SDK.on(DEVICE.CONNECT, (message: CoreMessage) => {
+        console.log('example get connect event: ', message);
+      });
+
+      SDK.on(DEVICE.DISCONNECT, (message: CoreMessage) => {
+        console.log('example get disconnect event: ', message);
+      });
+
+      registerListener = true;
+      console.log('Api payload: register sdk listeners');
+
+      return () => {
+        console.log('Api payload: remove all sdk listeners');
+
+        SDK.off(UI_EVENT, uiEventCallback);
+        registerListener = false;
+      };
+    }, [HardwareLowLevelSDK, SDK, onInputPinOnDeviceCallback])
+  );
+
   return (
     <View>
-      {showPinInput && (
-        <ReceivePin
-          value={pinValue}
-          onChange={val => setPinValue(val)}
-          onConfirm={val => onConfirmPin(val)}
-          onCancel={onPinCancelCallback}
-        />
-      )}
+      <ReceivePin
+        open={showPinInput}
+        onOpenChange={setShowPinInput}
+        onConfirm={val => onConfirmPin(val)}
+        onSwitchDevice={onInputPinOnDeviceCallback}
+        onCancel={onPinCancelCallback}
+      />
     </View>
   );
 }
--- packages/connect-examples/expo-example/src/components/ReceivePin.tsx
+++ packages/connect-examples/expo-example/src/components/ReceivePin.tsx
@@ -1,36 +1,156 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
-import { View, TextInput, Text, Button, Platform } from 'react-native';
+import { Dialog, SizableText, Stack, Text, Unspaced, XStack, YStack } from 'tamagui';
+import { Delete, X } from '@tamagui/lucide-icons';
+import { StyleSheet } from 'react-native';
+import { Button } from './ui/Button';
 
 type IReceivePinProps = {
-  value: string;
-  onChange: (value: string) => void;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
   onConfirm: (value: string) => void;
+  onSwitchDevice: () => void;
   onCancel: () => void;
 };
 
-export function ReceivePin({ value, onChange, onConfirm, onCancel }: IReceivePinProps) {
+const keyboardMap = ['7', '8', '9', /**/ '4', '5', '6', /**/ '1', '2', '3'];
+
+export function ReceivePin({
+  open,
+  onOpenChange,
+  onConfirm,
+  onSwitchDevice,
+  onCancel,
+}: IReceivePinProps) {
   const intl = useIntl();
 
+  const [val, setVal] = useState('');
+  const varMask = useMemo(
+    () =>
+      val
+        .split('')
+        .map(v => (v ? '•' : ''))
+        .join(''),
+    [val]
+  );
+
   return (
-    <View style={{ maxWidth: 300 }}>
-      <Text style={{ fontSize: 24 }}>Input Pin</Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-          padding: Platform.OS === 'web' ? 16 : 0,
-          margin: 12,
-        }}
-        onChangeText={text => onChange(text)}
-        value={value}
-      />
-      <Button
-        title={intl.formatMessage({ id: 'button__confirm' })}
-        onPress={() => onConfirm(value)}
-      />
-      <Button title={intl.formatMessage({ id: 'action__cancel' })} onPress={onCancel} />
-    </View>
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <Dialog.Portal padding="$4">
+        <Dialog.Overlay key="overlay" backgroundColor="$bgBackdrop" />
+        <Dialog.Content
+          key="content"
+          minWidth="100%"
+          minHeight={320}
+          $gtXs={{
+            minWidth: 480,
+          }}
+        >
+          <Dialog.Title>{intl.formatMessage({ id: 'title__input_pin' })}</Dialog.Title>
+
+          <YStack gap="$2">
+            <Stack
+              borderWidth={StyleSheet.hairlineWidth}
+              borderColor="$borderSubdued"
+              borderRadius="$2"
+              overflow="hidden"
+              style={{
+                borderCurve: 'continuous',
+              }}
+            >
+              <XStack
+                height="$12"
+                alignItems="center"
+                paddingHorizontal="$3"
+                borderBottomWidth={StyleSheet.hairlineWidth}
+                borderColor="$borderSubdued"
+                backgroundColor="$bgSubdued"
+              >
+                <Text selectable={false} paddingLeft="$6" textAlign="center" flex={1} fontSize={40}>
+                  {varMask}
+                </Text>
+                <Button
+                  variant="tertiary"
+                  onPress={() => {
+                    setVal(v => v.slice(0, -1));
+                  }}
+                >
+                  <Delete />
+                </Button>
+              </XStack>
+              <XStack flexWrap="wrap">
+                {keyboardMap.map((num, index) => (
+                  <Stack
+                    key={index}
+                    flexBasis="33.3333%"
+                    h="$14"
+                    borderRightWidth={StyleSheet.hairlineWidth}
+                    borderBottomWidth={StyleSheet.hairlineWidth}
+                    borderColor="$borderSubdued"
+                    justifyContent="center"
+                    alignItems="center"
+                    {...((index === 2 || index === 5 || index === 8) && {
+                      borderRightWidth: 0,
+                    })}
+                    {...((index === 6 || index === 7 || index === 8) && {
+                      borderBottomWidth: 0,
+                    })}
+                    hoverStyle={{
+                      bg: '$bgHover',
+                    }}
+                    pressStyle={{
+                      bg: '$bgActive',
+                    }}
+                    focusable
+                    focusStyle={{
+                      outlineColor: '$focusRing',
+                      outlineOffset: -2,
+                      outlineWidth: 2,
+                      outlineStyle: 'solid',
+                    }}
+                    onPress={() => setVal(v => v + num)}
+                  >
+                    <Stack
+                      width="$2.5"
+                      height="$2.5"
+                      borderRadius="$full"
+                      backgroundColor="$text"
+                    />
+                  </Stack>
+                ))}
+              </XStack>
+            </Stack>
+
+            <Dialog.Close asChild>
+              <Button size="large" onPress={() => onConfirm(val)}>
+                {intl.formatMessage({ id: 'action__confirm' })}
+              </Button>
+            </Dialog.Close>
+
+            <Dialog.Close asChild>
+              <Button onPress={onSwitchDevice}>
+                {intl.formatMessage({ id: 'action__switch_device' })}
+              </Button>
+            </Dialog.Close>
+          </YStack>
+          <Unspaced>
+            <Dialog.Close asChild>
+              <Button
+                onPress={onCancel}
+                variant="tertiary"
+                circular
+                width={32}
+                height={32}
+                position="absolute"
+                top="$3"
+                right="$3"
+              >
+                <X />
+              </Button>
+            </Dialog.Close>
+          </Unspaced>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
   );
 }
