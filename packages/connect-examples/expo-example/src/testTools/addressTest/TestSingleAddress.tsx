c6e3b0bd02daeb734c94e1ecf8ac675a2f8b2bfd
ByteZhang <quincysx@onekey.so>
2024-03-28 16:04:26 +0800

    fix: fix test error (#318)
    * chore: fix 24 one pubkey
    
    * fix: test case error

--- packages/connect-examples/expo-example/src/testTools/addressTest/TestSingleAddress.tsx
+++ packages/connect-examples/expo-example/src/testTools/addressTest/TestSingleAddress.tsx
@@ -172,6 +172,7 @@ function ExecuteView({ testCases }: { testCases: AddressTestCase[] }) {
       const { params } = item;
       const requestParams = {
         ...params,
+        showOnOneKey,
         passphraseState: currentTestCase?.extra?.passphraseState,
         useEmptyPassphrase: !currentTestCase?.extra?.passphrase,
       };
