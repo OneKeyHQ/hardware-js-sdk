4320051417cccf35b1f47c3d7fc0927a7f4d94ec
ByteZhang <quincysx@onekey.so>
2024-04-24 20:35:14 +0800

    chore: update sdk version

--- packages/connect-examples/expo-example/src/constants/connect.ts
+++ packages/connect-examples/expo-example/src/constants/connect.ts
@@ -1,2 +1,2 @@
-export const CONNECT_SRC = 'https://jssdk.onekey.so/1.0.0-alpha.3/';
+export const CONNECT_SRC = 'https://jssdk.onekey.so/1.0.0-alpha.5/';
 // export const CONNECT_SRC = 'https://localhost:8087/';
