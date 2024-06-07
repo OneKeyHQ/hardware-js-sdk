3c2e02f19838c7e37c11b881788176e2c7011f55
ByteZhang <quincysx@onekey.so>
2024-06-07 18:29:17 +0800

    feat: add auto update

---
+++ packages/connect-examples/electron-example/src/config.ts
@@ -0,0 +1,10 @@
+export const ipcMessageKeys = {
+  // Updater
+  UPDATE_AVAILABLE: 'update/available',
+  UPDATE_DOWNLOADED: 'update/downloaded',
+  UPDATE_RESTART: 'update/restartApp',
+
+  APP_RELOAD_BRIDGE_PROCESS: 'app/reloadBridgeProcess',
+
+  INJECT_ONEKEY_DESKTOP_GLOBALS: 'inject/onekeyDesktop',
+};
