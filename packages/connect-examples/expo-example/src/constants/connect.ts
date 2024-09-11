e4cfc2751a3dbf71ac149d854dc9d048552d875b
ByteZhang <quincysx@onekey.so>
2024-09-11 17:10:37 +0800

    chore: release version 1.0.10

--- packages/connect-examples/expo-example/src/constants/connect.ts
+++ packages/connect-examples/expo-example/src/constants/connect.ts
@@ -1,2 +1,2 @@
-export const CONNECT_SRC = 'https://jssdk.onekey.so/1.0.10-alpha.10/';
+export const CONNECT_SRC = 'https://jssdk.onekey.so/1.0.10/';
 // export const CONNECT_SRC = 'https://localhost:8087/';
