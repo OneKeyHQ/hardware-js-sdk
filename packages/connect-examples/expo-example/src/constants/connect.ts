5597464cdaedd97ae8219dd85f7f039c6ce1c6de
蔡凯升 <kaisheng.cai@onekey.so>
2024-10-09 18:15:19 +0800

    fix: use example with 1.0.11

--- packages/connect-examples/expo-example/src/constants/connect.ts
+++ packages/connect-examples/expo-example/src/constants/connect.ts
@@ -1,3 +1,1 @@
-import { version } from '../../package.json';
-
-export const CONNECT_SRC = process.env.CONNECT_SRC || `https://jssdk.onekey.so/${version}`;
+export const CONNECT_SRC = process.env.CONNECT_SRC || `https://jssdk.onekey.so/1.0.11`;
