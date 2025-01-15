6ffe286cb4c87d7e0ec0c99db67e2c2e36105acc
ByteZhang <ByteZhang@protonmail.com>
2025-01-15 18:06:48 +0800

    fix: ton sign tx initstate

--- packages/hd-transport/src/types/messages.ts
+++ packages/hd-transport/src/types/messages.ts
@@ -3234,6 +3234,31 @@ export type NEMDecryptedMessage = {
   payload: string;
 };
 
+// NeoGetAddress
+export type NeoGetAddress = {
+  address_n: number[];
+  show_display?: boolean;
+};
+
+// NeoAddress
+export type NeoAddress = {
+  address?: string;
+  public_key?: string;
+};
+
+// NeoSignTx
+export type NeoSignTx = {
+  address_n: number[];
+  raw_tx: string;
+  network_magic?: number;
+};
+
+// NeoSignedTx
+export type NeoSignedTx = {
+  public_key: string;
+  signature: string;
+};
+
 // NervosGetAddress
 export type NervosGetAddress = {
   address_n: number[];
@@ -3989,12 +4014,21 @@ export type TonSignMessage = {
   ext_ton_amount: UintType[];
   ext_payload: string[];
   jetton_amount_bytes?: string;
+  init_data_initial_chunk?: string;
+  init_data_length?: number;
+  signing_message_repr?: string;
+};
+
+// TonTxAck
+export type TonTxAck = {
+  init_data_chunk: string;
 };
 
 // TonSignedMessage
 export type TonSignedMessage = {
   signature?: string;
   signning_message?: string;
+  init_data_length?: number;
 };
 
 // TonSignProof
@@ -4135,31 +4169,6 @@ export enum CommandFlags {
   Default = 0,
   Factory_Only = 1,
 }
-
-// NeoGetAddress
-export type NeoGetAddress = {
-  address_n: number[];
-  show_display?: boolean;
-};
-
-// NeoAddress
-export type NeoAddress = {
-  address?: string;
-  public_key?: string;
-};
-
-// NeoSignTx
-export type NeoSignTx = {
-  address_n: number[];
-  raw_tx: string;
-  network_magic?: number;
-};
-
-// NeoSignedTx
-export type NeoSignedTx = {
-  public_key: string;
-  signature: string;
-};
 
 // custom connect definitions
 export type MessageType = {
@@ -4673,6 +4682,7 @@ export type MessageType = {
   TonGetAddress: TonGetAddress;
   TonAddress: TonAddress;
   TonSignMessage: TonSignMessage;
+  TonTxAck: TonTxAck;
   TonSignedMessage: TonSignedMessage;
   TonSignProof: TonSignProof;
   TonSignedProof: TonSignedProof;
