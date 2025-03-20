29ce56f09155604ca181658229ecdfdcac2be1bf
Leon <lixiao@onekey.so>
2025-03-20 23:49:32 +0800

    chore: lint

--- packages/hd-ble-sdk/src/index.ts
+++ packages/hd-ble-sdk/src/index.ts
@@ -168,7 +168,7 @@ const call = async (params: any) => {
 };
 
 const updateSettings = () => Promise.resolve(true);
-const switchTransport = () => Promise.resolve(true);
+const switchTransport = () => Promise.resolve({ success: true });
 
 const HardwareBleSdk = HardwareSdk({
   eventEmitter,
--- packages/hd-common-connect-sdk/src/index.ts
+++ packages/hd-common-connect-sdk/src/index.ts
@@ -159,7 +159,7 @@ const call = async (params: any) => {
 };
 
 const updateSettings = () => Promise.resolve(true);
-const switchTransport = () => Promise.resolve(true);
+const switchTransport = () => Promise.resolve({ success: true });
 
 const HardwareCommonConnectSdk = HardwareSdk({
   eventEmitter,
