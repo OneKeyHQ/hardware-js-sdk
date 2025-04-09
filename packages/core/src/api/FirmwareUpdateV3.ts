b48e75ed6e76824f709b7b0f3f2ff5c5248ed0cf
蔡凯升 <kaisheng.cai@onekey.so>
2025-04-09 17:54:22 +0800

    fix: unexpectedError in fw

--- packages/core/src/api/FirmwareUpdateV3.ts
+++ packages/core/src/api/FirmwareUpdateV3.ts
@@ -280,8 +280,22 @@ export default class FirmwareUpdateV3 extends FirmwareUpdateBaseMethod<FirmwareU
         path: '0:updates',
       });
     } catch (error) {
-      if (error.errorCode && error.errorCode === HardwareErrorCode.ActionCancelled) {
-        throw error;
+      if (error.errorCode) {
+        const unexpectedError = [
+          HardwareErrorCode.ActionCancelled,
+          HardwareErrorCode.BleDeviceNotBonded,
+          HardwareErrorCode.BleServiceNotFound,
+          HardwareErrorCode.BlePermissionError,
+          HardwareErrorCode.BleLocationError,
+          HardwareErrorCode.BleDeviceBondError,
+          HardwareErrorCode.BleCharacteristicNotifyError,
+          HardwareErrorCode.BleTimeoutError,
+          HardwareErrorCode.BleWriteCharacteristicError,
+          HardwareErrorCode.WebDeviceNotFoundOrNeedsPermission,
+        ];
+        if (unexpectedError.includes(error.errorCode)) {
+          throw error;
+        }
       }
       Log.error('triggerFirmwareUpdateEmmc error: ', error);
     }
