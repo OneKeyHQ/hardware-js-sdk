50efd8d49312b796a1a509a0411adae4162b1611
蔡凯升 <kaisheng.cai@onekey.so>
2025-05-15 19:36:04 +0800

    fix: deviceVerify in factory

--- packages/core/src/api/device/DeviceVerify.ts
+++ packages/core/src/api/device/DeviceVerify.ts
@@ -8,10 +8,12 @@ import { validateParams, validateResult } from '../helpers/paramsValidator';
 import { getDeviceType } from '../../utils';
 import { DeviceModelToTypes } from '../../types';
 import type { DeviceVerifySignature } from '../../types';
+import { UI_REQUEST } from '../../events';
 
 export default class DeviceVerify extends BaseMethod<BixinVerifyDeviceRequest> {
   init() {
     this.useDevicePassphraseState = false;
+    this.notAllowDeviceMode = [UI_REQUEST.BOOTLOADER, UI_REQUEST.INITIALIZE];
 
     // check payload
     validateParams(this.payload, [{ name: 'dataHex', type: 'hexString' }]);
