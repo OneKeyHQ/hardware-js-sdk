50660eab9c9515b4077701ef3e487cbe23520c18
蔡凯升 <kaisheng.cai@onekey.so>
2025-06-12 16:38:05 +0800

    fix: build

--- packages/connect-examples/expo-example/webpack.config.js
+++ packages/connect-examples/expo-example/webpack.config.js
@@ -1,32 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-const path = require('path');
 
 // Expo CLI will await this method so you can optionally return a promise.
 module.exports = async function (env, argv) {
-  // 获取基础的 Expo Webpack 配置
-  const config = await createExpoWebpackConfigAsync(
-    {
-      ...env,
-      // 告诉 Expo 我们正在使用 monorepo 结构
-      // 这会启用一些默认的 monorepo 支持
-      projectRoot: path.resolve(__dirname, '../../..'),
-    },
-    argv
-  );
-
-  // --- 手动修复模块解析 ---
-  // 1. 将 monorepo 的根 node_modules 添加到解析路径中
-  config.resolve.modules.push(path.resolve(__dirname, '../../../node_modules'));
-
-  // 2. 确保 Babel 会编译 monorepo 中的其他包
-  // 这对于使用 TypeScript 或 JSX 语法的本地包是必需的
-  config.module.rules.push({
-    test: /\.(js|jsx|ts|tsx)$/,
-    // `packages` 目录是所有本地包的根目录
-    include: path.resolve(__dirname, '../../../packages'),
-    use: 'babel-loader',
-  });
+  const config = await createExpoWebpackConfigAsync(env, argv);
 
   // 只为我们自己的代码启用 source map
   config.module.rules = config.module.rules.filter(rule => {
