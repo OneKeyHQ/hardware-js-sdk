fe5f34255d4a549a44c58f347d48f546a847c220
ByteZhang <ByteZhang@protonmail.com>
2025-06-26 10:23:58 +0800

    fix: batch limit

--- packages/core/src/utils/deviceFeaturesUtils.ts
+++ packages/core/src/utils/deviceFeaturesUtils.ts
@@ -136,6 +136,9 @@ export const supportBatchPublicKey = (
   if (!!options?.includeNode && deviceType === EDeviceType.Classic) {
     return semver.gte(currentVersion, '3.10.0');
   }
+  if (options?.includeNode) {
+    return false;
+  }
 
   // support batch get public key
   if (deviceType === EDeviceType.Touch || deviceType === EDeviceType.Pro) {
