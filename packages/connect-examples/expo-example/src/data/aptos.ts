774e31bf623b82d4e69f4556027b18ea11385237
蔡凯升 <kaisheng.cai@onekey.so>
2025-08-24 12:44:07 +0800

    feat: support eip 7702

--- packages/connect-examples/expo-example/src/data/aptos.ts
+++ packages/connect-examples/expo-example/src/data/aptos.ts
@@ -85,6 +85,19 @@ const api: PlaygroundProps[] = [
     ],
   },
   {
+    method: 'aptosSignInMessage',
+    presupposes: [
+      {
+        title: 'Sign message',
+        value: {
+          path: "m/44'/637'/0'/0'/0'",
+          payload:
+            'localhost:3000 wants you to sign in with your Aptos account:\\n0xeb2d1d9bbfca9d892e124e858f1dc449935a3f785f8860892e03fb9820db1839\\n\\nSigning into demo application\\n\\nURI: https://dapp-example.onekeytest.com\\nVersion: 1.0.0\\nNonce: 0.72024fabf0bbd\\nIssued At: 2025-08-20T08:58:22.851Z\\nExpiration Time: 2025-08-21T08:58:22.851Z\\nNot Before: 2025-08-20T08:58:22.851Z\\nRequest ID: abc\\nChain ID: aptos--1\\nResources:\\n- resource.1\\n- resource.2',
+        },
+      },
+    ],
+  },
+  {
     method: 'aptosSignTransaction',
 
     presupposes: [
