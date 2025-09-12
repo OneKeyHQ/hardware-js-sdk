7fe71625a81bba6f13aa972bd3cc07ec2549b033
ByteZhang <ByteZhang@protonmail.com>
2025-09-12 16:34:41 +0800

    Revert "feat: support blur resource (#550)" (#571)
    This reverts commit 3c9d959d8ca3285b8020722eca640e065867e116.

--- packages/core/src/api/device/DeviceUploadResource.ts
+++ packages/core/src/api/device/DeviceUploadResource.ts
@@ -9,6 +9,7 @@ import { DeviceModelToTypes, DeviceUploadResourceParams } from '../../types';
 import { BaseMethod } from '../BaseMethod';
 import { validateParams } from '../helpers/paramsValidator';
 import { hexToBytes } from '../helpers/hexUtils';
+import { createUiMessage, UI_REQUEST } from '../../events';
 import { getDeviceType, getDeviceFirmwareVersion } from '../../utils';
 import { PROTO } from '../../constants';
 
@@ -16,13 +17,6 @@ export default class DeviceUploadResource extends BaseMethod<ResourceUpload> {
   paramsData = {
     data: new Uint8Array(),
     thumbnailData: new Uint8Array(),
-    blurData: new Uint8Array(),
-  };
-
-  private uploadProgress = {
-    totalBytes: 0,
-    uploadedBytes: 0,
-    currentFile: 'main' as 'main' | 'thumbnail' | 'blur',
   };
 
   getVersionRange() {
@@ -58,27 +52,19 @@ export default class DeviceUploadResource extends BaseMethod<ResourceUpload> {
       { name: 'suffix', type: 'string', required: true },
       { name: 'dataHex', type: 'string', required: true },
       { name: 'thumbnailDataHex', type: 'string', required: true },
-      { name: 'blurDataHex', type: 'hexString', required: true },
       { name: 'resType', type: 'number', required: true },
       { name: 'nftMetaData', type: 'string' },
       { name: 'fileNameNoExt', type: 'string' },
     ]);
 
-    const { suffix, dataHex, thumbnailDataHex, blurDataHex, resType, nftMetaData } = this
+    const { suffix, dataHex, thumbnailDataHex, resType, nftMetaData } = this
       .payload as DeviceUploadResourceParams;
 
     // init params
     this.paramsData = {
-      data: new Uint8Array(hexToBytes(dataHex)),
-      thumbnailData: new Uint8Array(hexToBytes(thumbnailDataHex)),
-      blurData: new Uint8Array(hexToBytes(blurDataHex)),
+      data: hexToBytes(dataHex),
+      thumbnailData: hexToBytes(thumbnailDataHex),
     };
-
-    this.uploadProgress.totalBytes =
-      this.paramsData.data.byteLength +
-      this.paramsData.thumbnailData.byteLength +
-      this.paramsData.blurData.byteLength;
-    this.uploadProgress.uploadedBytes = 0;
 
     const fileHash = bytesToHex(blake2s(this.payload.dataHex)).slice(0, 8);
     const file_name_no_ext = isEmpty(this.payload.fileNameNoExt)
@@ -89,44 +75,16 @@ export default class DeviceUploadResource extends BaseMethod<ResourceUpload> {
       extension: suffix,
       data_length: this.paramsData.data.byteLength,
       zoom_data_length: this.paramsData.thumbnailData.byteLength,
-      blur_data_length: this.paramsData.blurData.byteLength,
       res_type: resType,
       nft_meta_data: nftMetaData,
       file_name_no_ext,
     };
   }
 
-  private getDataChunk(sourceData: Uint8Array, offset: number, length: number): Uint8Array {
-    const endOffset = Math.min(offset + length, sourceData.byteLength);
-
-    return sourceData.subarray(offset, endOffset);
-  }
-
-  private updateProgress(chunkSize: number, requestType: string) {
-    this.uploadProgress.uploadedBytes += chunkSize;
-
-    if (requestType === 'ResourceRequest') {
-      this.uploadProgress.currentFile = 'main';
-    } else if (requestType === 'ZoomRequest') {
-      this.uploadProgress.currentFile = 'thumbnail';
-    } else {
-      this.uploadProgress.currentFile = 'blur';
-    }
-
-    const progress = Math.round(
-      (this.uploadProgress.uploadedBytes / this.uploadProgress.totalBytes) * 100
-    );
-
-    if (process.env.NODE_ENV === 'development') {
-      console.log(`Upload progress: ${progress}% (${this.uploadProgress.currentFile})`);
-    }
-  }
-
   processResourceRequest = async (
     res:
       | TypedResponseMessage<'ResourceRequest'>
       | TypedResponseMessage<'ZoomRequest'>
-      | TypedResponseMessage<'BlurRequest'>
       | TypedResponseMessage<'Success'>
   ): Promise<Success> => {
     if (res.type === 'Success') {
@@ -134,32 +92,22 @@ export default class DeviceUploadResource extends BaseMethod<ResourceUpload> {
     }
 
     const { offset, data_length } = res.message;
-    const { data, thumbnailData, blurData } = this.paramsData;
+    const { data, thumbnailData } = this.paramsData;
 
     if (offset === undefined) {
       throw new Error('offset is undefined');
     }
 
-    let sourceData: Uint8Array;
-
-    switch (res.type) {
-      case 'ResourceRequest':
-        sourceData = data;
-        break;
-      case 'BlurRequest':
-        sourceData = blurData;
-        break;
-      case 'ZoomRequest':
-        sourceData = thumbnailData;
-        break;
-      default:
-        throw new Error('Invalid request type');
+    let payload: Uint8Array;
+    if (res.type === 'ResourceRequest') {
+      payload = new Uint8Array(data.slice(offset, Math.min(offset + data_length, data.byteLength)));
+    } else {
+      payload = new Uint8Array(
+        thumbnailData.slice(offset, Math.min(offset + data_length, thumbnailData.byteLength))
+      );
     }
 
-    const payload = this.getDataChunk(sourceData, offset, data_length);
     const digest = blake2s(payload);
-
-    this.updateProgress(payload.byteLength, res.type);
 
     const resourceAckParams = {
       data_chunk: bytesToHex(payload),
@@ -168,7 +116,7 @@ export default class DeviceUploadResource extends BaseMethod<ResourceUpload> {
 
     const response = await this.device.commands.typedCall(
       'ResourceAck',
-      ['ResourceRequest', 'ZoomRequest', 'BlurRequest', 'Success'],
+      ['ResourceRequest', 'ZoomRequest', 'Success'],
       resourceAckParams
     );
     return this.processResourceRequest(response);
@@ -181,9 +129,11 @@ export default class DeviceUploadResource extends BaseMethod<ResourceUpload> {
 
     const res = await this.device.commands.typedCall(
       'ResourceUpload',
-      ['ResourceRequest', 'ZoomRequest', 'BlurRequest', 'Success'],
+      ['ResourceRequest', 'ZoomRequest', 'Success'],
       this.params
     );
+
+    this.postMessage(createUiMessage(UI_REQUEST.CLOSE_UI_WINDOW));
 
     return this.processResourceRequest(res);
   }
